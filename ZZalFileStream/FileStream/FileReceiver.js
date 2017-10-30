767674bf8aa81f52b39a1b639b0d243fefcd6f6d
YOOKANGHYEON-PC\yoo kang hyeon <yoo5459@gmail.com>
2017-10-30 14:33:11 +0900

    node JS 파일스트림 추가

---
+++ ZZalFileStream/FileStream/FileReceiver.js
@@ -0,0 +1,24 @@
+﻿var formidable = require('formidable');
+var fs = require('fs');
+
+// Form type file Upload 요청 처리
+var filereciever = function(req) {
+    var fileform = new formidable.IncomingForm();
+
+    fileform.encoding = 'utf-8';
+
+	fileform.parse(req, function (err, fields, files){
+		var fileType = files.type.split('/').pop();
+		if(fileType === 'jpg' || fileType === 'png' || fileType === 'jpeg' || fileType === 'gif'){
+            var oldpath = files.filetoupload.path;
+            var newpath = './imgs/' + files.filetoupload.name;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+            });
+        }
+        else {
+            fs.unlink(files.files.name);
+            throw 'Invaild File Type';
+        }
+	});
+};
---
+++ ZZalFileStream/server.js
@@ -0,0 +1,10 @@
+import './FileStream/FileReceiver.js';
+
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
