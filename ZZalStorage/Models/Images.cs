8397ac19d6b51f96f5217e1c32d18904562392c4
YOOKANGHYEON-PC\yoo kang hyeon <yoo5459@gmail.com>
2017-10-31 16:39:29 +0900

    lib 추가, 모델추가

---
+++ ZZalStorage/Models/Images.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZZalStorage.Models
+{
+    public class Images
+    {        
+        public int ID { get; set; }
+        public string FileName { get; set; }
+        public DateTime AddDate { get; set; }
+        public ICollection<string> Tags { get; set; }
+    }
+}
